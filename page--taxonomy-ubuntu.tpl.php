ce55acbc33fd0c6210b9e783a3cf9304da4a7791
unknown <OSORIO@web-9.laopinion>
2019-07-05 08:15:40 -0500

    Subí proyecto Ubunto a Github

---
+++ page--taxonomy-ubuntu.tpl.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+  	<link rel="stylesheet" href="<?php print base_path(); ?>sites/default/themes/op/ubuntu/css/normalize.css">
+  	<link rel="stylesheet" href="<?php print base_path(); ?>sites/default/themes/op/ubuntu/css/styles.css">
+  	<link rel="icon" href="<?php print base_path(); ?>sites/default/themes/op/ubuntu/images/alu.png" />
+  	<meta property="og:locale:alternate" content="es_CO" />
+  	<meta property="og:locale" content="es_CO" />
+  	<link rel="canonical" href="http://www.laopinion.com.co/ubuntu"/>
+  	<link rel="image_src" href="http://www.laopinion.com.co/sites/default/themes/op/ubuntu/images/compartir.jpg"/>
+
+  	<meta name="ROBOTS" content="FOLLOW">
+  	<meta name="author" content="Diario la Opinión" />
+  	<meta name="google" content="notranslate" />
+	<meta name="geo.region" content="CO" />
+	<meta name="geo.placename" content="Colombia" />
+	<meta name="language" content="spanish" />
+	
+	<meta itemprop="name" content="Academia de líderes de ubuntu" />
+	<meta itemprop="description" name="description" content="La Academia Ubuntu es un proyecto de capacitación de jóvenes con alto potencial de liderazgo, provenientes de contextos de exclusión social o con aptitud para trabajar allí, con el objetivo de facilitar el desarrollo de sus capacidades personales, de modo que mejor puedan intervenir en esos contextos."/>
+   
+    <title>Academia de líderes Ubuntu</title>
+
+</head>
+
+<body>
+	<header>
+		<div class="encabezado">
+			<img src="<?php print base_path(); ?>sites/default/themes/op/ubuntu/images/ubuntu.png">
+		</div>
+	</header>
+
+	<div class="container">
+		<article>
+			<div class="texto">
+				“Ubuntu es la esencia del ser humano. Nos habla del hecho de que, solo, nadie puede existir como ser humano. Recuerda nuestra interdependencia. Nadie puede ser Humano por sí mismo, pero cuando tiene esta calidad - Ubuntu - es conocido por su generosidad. Nosotros pensamos a menudo sólo como individuos, separados unos de otros. Pero estamos conectados. Lo que cada uno de nosotros hace, afecta al mundo entero.” <br>
+				(Desmond Tutu, 2008)
+			</div>
+			
+			<p class="info">
+				La Academia Ubuntu es un proyecto de capacitación de jóvenes con alto potencial de liderazgo, provenientes de contextos de exclusión social o con aptitud para trabajar allí, con el objetivo de facilitar el desarrollo de sus capacidades personales, de modo que mejor puedan intervenir en esos contextos.
+			</p>
+		</article>
+		<article>
+			<div class="form">
+				<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe2txBpHoRkWK7tka-6ln8ExLlxts79cNN4ZxMOR-9TksE4EQ/viewform?embedded=true" width="70%" height="1300" frameborder="0" marginheight="0" marginwidth="15%">Cargando…</iframe>
+			</div>
+
+		</article>
+		
+		<footer>
+			<div class="grid">
+				<div class="item">
+					<img src="<?php print base_path(); ?>sites/default/themes/op/ubuntu/images/USAID.svg">
+				</div>
+				<div class="item">
+					<img src="<?php print base_path(); ?>sites/default/themes/op/ubuntu/images/OPINION_01.svg">
+				</div>
+				<div class="itemopinion">
+					<img src="<?php print base_path(); ?>sites/default/themes/op/ubuntu/images/AVlogo.svg">
+				</div>
+			</div>
+		</footer>
+	</div>
+</body>
+</html>
